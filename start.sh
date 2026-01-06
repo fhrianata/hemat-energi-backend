d9ba5694846538f5764dfc5de5f7b384200428a7
FachriPutraAnata <fachrianata123@gmail.com>
2026-01-07 04:51:29 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
+
+apachectl -D FOREGROUND
